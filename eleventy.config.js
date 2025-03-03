2d65d20597ccf2047909e3f4e2fe3150132f4412
Myocardinal <development@myocardinal.com>
2025-03-02 19:24:10 -0500

    UGLY WEBSITE DO NOT LOOK

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,10 +2,21 @@ export default function (eleventyConfig) {
     // Output directory: _site
 
     // Copy `css/` to `_site/css/`
-    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("css/");
 
+    // Copy CNAME for github pages to point to my domain
+    eleventyConfig.addPassthroughCopy({"config/CNAME": "CNAME"})
 
+    eleventyConfig.setServerOptions({
+        mimeTypes: {
+            "css": "text/css"
+        }
+    })
 
-    eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
+    return {
+        templateFormats: ['njk', 'md', 'html', 'css'],
+        dir: {
+
+        }
+    }
 };
-
